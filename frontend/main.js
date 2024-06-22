64d67ae513f7ff38fe692c9e05da22c5fe311ff4
Jorge Ayala <jorge.ayalavalenzuela7@gmail.com>
2024-06-22 13:29:12 -0400

    added physics

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,41 @@
 import './style.css'
-import Phaser from 'phaser'
+import Phaser, { Physics } from 'phaser'
+
 
 const sizes = {
     width: 500,
     height: 500
 }
 
+const speedDown = 300
+
+class GameScene extends Phaser.Scene{
+    constructor(){
+        super("scene-game")
+    }
+
+    preload(){
+        this.load.image("background", "/assets/background.jpeg")
+    }
+    create(){
+        this.add.image(0,0,"background").setOrigin(0,0)
+    }
+    update(){}
+}
+
 const config = {
     type: Phaser.WEBGL,
     width: sizes.width,
     height: sizes.height,
-    canvas:gameCanvas
+    canvas:gameCanvas,
+    Physics: {
+        default: "arcade",
+        arcade:{
+            gravity: {y: speedDown},
+            debug: true
+        }
+    },
+    scene: [GameScene]
 }
 
 const game = new Phaser.Game(config)
