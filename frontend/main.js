fa85e144a06aa163ac8acf7b0ba492b0741ef65c
Jorge Ayala <jorge.ayalavalenzuela7@gmail.com>
2024-06-23 11:02:06 -0400

    added worm

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import Phaser, { Physics } from 'phaser'
+import Phaser from 'phaser'
 
 
 const sizes = {
@@ -12,13 +12,18 @@ const speedDown = 300
 class GameScene extends Phaser.Scene{
     constructor(){
         super("scene-game")
+        this.player
     }
 
     preload(){
         this.load.image("background", "/assets/background.jpeg")
+        this.load.image("worm", "/assets/wormsprite.png" )
     }
     create(){
         this.add.image(0,0,"background").setOrigin(0,0)
+        this.player = this.physics.add.image(0, sizes.height-100, "worm").setOrigin(0, 0)
+        this.player.setImmovable(true)
+        this.player.body.allowGravity = false
     }
     update(){}
 }
@@ -28,7 +33,7 @@ const config = {
     width: sizes.width,
     height: sizes.height,
     canvas:gameCanvas,
-    Physics: {
+    physics: {
         default: "arcade",
         arcade:{
             gravity: {y: speedDown},
